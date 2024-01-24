267e54f26b7990e3a8db6b4af31166ed39076ab0
nargizskakova-04 <skakovasaule7@gmail.com>
2024-01-25 02:19:00 +0600

    UV API

--- ass2/weather.js
+++ ass2/weather.js
@@ -14,22 +14,44 @@ app.post('/', function(req, res){
     const unit = "metric";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
 
-    https.get(url, function(response){
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const feelsLike = weatherData.main.feels_like;
-            const humidity = weatherData.main.humidity;
-            const pressure = weatherData.main.pressure;
-            const windSpeed = weatherData.wind.speed;
-            const countryCode = weatherData.sys.country;
-            const latitude = weatherData.coord.lat;
-            const longitude = weatherData.coord.lon;
+    https.get(url, function(weatherResponse){
+        let weatherData = '';
+        weatherResponse.on("data", function(data){
+            weatherData += data;
+        });
+        weatherResponse.on("end", function(){
+            const weatherJson = JSON.parse(weatherData);
+            const temp = weatherJson.main.temp;
+            const weatherDescription = weatherJson.weather[0].description;
+            const icon = weatherJson.weather[0].icon;
+            const feelsLike = weatherJson.main.feels_like;
+            const humidity = weatherJson.main.humidity;
+            const pressure = weatherJson.main.pressure;
+            const windSpeed = weatherJson.wind.speed;
+            const countryCode = weatherJson.sys.country;
+            const uvApiKey = 'openuv-3xsowrlrs61fbl-io'
+            const latitude = weatherJson.coord.lat;
+            const longitude = weatherJson.coord.lon;
+            const uvUrl = `https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}`;
+            const uvOptions = {
+                headers: {'x-access-token': uvApiKey}
+            };
+            https.get(uvUrl, uvOptions, function(uvResponse){
+                let uvData = '';
+                uvResponse.on('data', function(chunk){
+                    uvData += chunk;
+                });
 
-            const query = `?city=${city}&temp=${temp}&desc=${weatherDescription}&icon=${icon}&feelsLike=${feelsLike}&humidity=${humidity}&pressure=${pressure}&windSpeed=${windSpeed}&countryCode=${countryCode}&lat=${latitude}&lon=${longitude}`;
-            res.redirect('/weather' + query);
+                uvResponse.on('end', function(){
+                    const uvJson = JSON.parse(uvData);
+                    const uvIndex = uvJson.result.uv;
+
+                    const query = `?city=${city}&temp=${temp}&desc=${weatherDescription}&icon=${icon}&feelsLike=${feelsLike}&humidity=${humidity}&pressure=${pressure}&windSpeed=${windSpeed}&countryCode=${countryCode}&lat=${latitude}&lon=${longitude}&uvIndex=${uvIndex}`;
+                    res.redirect('/weather' + query);
+                });
+            }).on('error', function(e){
+                console.error(`Ошибка при запросе к OpenUV: ${e.message}`);
+            });
         });
     });
 });
@@ -41,3 +63,5 @@ app.get('/weather', function(req, res){
 app.listen(3000, function(){
     console.log('Server is running on port 3000');
 });
+
+
